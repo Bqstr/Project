53b4f6a31a2a5544d687e1f922c83d3979a8de2e
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-03-15 10:47:40 +0600

    Delete src/main/java/com directory

--- src/main/java/com/example/billboardproject/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.billboardproject.model;
-
-public enum Role  {
-    MANAGER, USER
-
-}
--- src/main/java/com/example/billboardproject/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.billboardproject.service;
-
-import com.example.billboardproject.model.Role;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface RoleService {
-    Role getRoleByRoleName(String role);
-}
